40da4e3fcf937c2d7a254d64c6ef15fb7f60d15c
taewan625 <taewan1997625@gmail.com>
2023-09-03 18:05:37 +0900

    refector weater API

--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,19 @@
-//API-KEY 는 전역변수를 사용하여 gitignore 시킴. 연동을 위해서 key가 있는 js file을 html에 script로 연동만 하면 됨
-function onGoOk(position) {
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const location = document.querySelector("#weather span:last-child");
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp} ºC`;
-      location.innerText = data.name;
-    }); //fetch를 이용해서 network 창에서 url에 관한 정보를 얻을 수 있다
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGoError() {
   alert("can't find your location");
 }
 
-navigator.geolocation.getCurrentPosition(onGoOk, onGoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGoError);
